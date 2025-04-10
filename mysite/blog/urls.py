c34ef4a25baccf10352b5f799c5cae6f57a8afa2
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-04-10 22:19:15 +0300

    urls

---
+++ mysite/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+#from ..mysite.urls import urlpatterns
+
+app_name = 'blog'
+
+urlpatterns = [
+    #представления поста
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+]
