284e41ac8ba95ba350b9a53164ccddb308710594
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-04-19 03:30:48 +0300

    обработка ошибки страницы вне диапазона

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,14 +2,17 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 def post_list(request):
     post_list = Post.published.all()
     #Постраничная разбивка
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:#Если указанная страница вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',{'posts':posts})
 
 # Create your views here.
