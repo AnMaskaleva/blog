efb7e353e5c736241c9bdc8438236c379b48a3ed
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-05-04 20:42:08 +0300

    добавление комментария в детальную инфу

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -24,8 +24,10 @@ def post_list(request):
 # Create your views here.
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, 'blog/post/detail.html',{'post':post})
+    return render(request, 'blog/post/detail.html',{'post':post, 'comments':comments, 'form':form})
 
 class PostListView(ListView):
     queryset = Post.published.all()
