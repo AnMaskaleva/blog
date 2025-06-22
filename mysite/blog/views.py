45b0dbd3fde7b2d02ca7c5d066e7aac6d7f62d44
AnitaMaskaleva <maskaleva.an@gmail.com>
2025-06-22 22:47:24 +0300

    шаблонные теги, шаблонные фильтры, карта сайта, новостная лента

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,9 +7,15 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     #Постраничная разбивка
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -19,7 +25,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:#Если указанная страница вне диапазона, то выдать последнюю страницу
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html',{'posts':posts})
+    return render(request, 'blog/post/list.html',{'posts':posts, 'tag':tag})
 
 # Create your views here.
 def post_detail(request, year, month, day, post):
@@ -27,7 +33,12 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
-    return render(request, 'blog/post/detail.html',{'post':post, 'comments':comments, 'form':form})
+    #Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html',{'post':post, 'comments':comments, 'form':form, 'similar_posts':similar_posts})
 
 class PostListView(ListView):
     queryset = Post.published.all()
